23e18e8b477d853abede40606aa8c53757cf0645
Daniel Silva <devdanielsilva@gmail.com>
2023-09-28 13:39:38 -0300

    Create media-de-aluno.php

---
+++ media-de-aluno.php
@@ -0,0 +1,14 @@
+<?php 
+$nota1 = 6;
+$nota2 = 8;
+$media = ($nota1 + $nota2)/2;
+/*Podemos usar o print ou o echo para apresentar os resultados na tela. Tem outros... */
+print 'A média do aluno é ' .$media . '<br>';
+//Podemos usar comandos html na linha de código, entre aspas simples ou duplas, assim como fizemos no print.
+
+echo "A nota 1 = $nota1 e a nota 2 = $nota2 <br> A média é $media";
+
+
+
+
+?>
