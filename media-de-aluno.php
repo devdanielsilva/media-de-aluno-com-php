b76a0ecea98463ba3e45522f4267caa367064fc7
Daniel Silva <devdanielsilva@gmail.com>
2023-10-25 14:50:03 -0300

    Update media-de-aluno.php

--- media-de-aluno.php
+++ media-de-aluno.php
@@ -1,12 +1,12 @@
 <?php 
-$nota1 = 6;//Toda variável em php, com $ cifrão na frente.
+$nota1 = 6;//Toda variável em php, começa com o $ cifrão na frente.
 $nota2 = 8;
 $media = ($nota1 + $nota2)/2;
 /*Podemos usar o print ou o echo para apresentar os resultados na tela. Tem outros... */
 print 'A média do aluno é ' .$media . '<br>';
 //Podemos usar comandos html na linha de código, entre aspas simples ou duplas, assim como fizemos no print.
 
-echo "A nota 1 = $nota1 e a nota 2 = $nota2 <br> A média é $media";
+echo "A nota 1 =  $nota1 e a nota 2 =  $nota2 <br> A média é <strong>$media</strong>";
 
 
 
